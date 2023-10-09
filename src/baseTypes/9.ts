d7d11452c0f54cdc0a9b19818edbc097a58da067
M-Tyshchenko <m.tish2406@gmail.com>
2023-10-09 18:37:50 +0300

    Update 9.ts

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -5,8 +5,8 @@ type Page = {
   title: string,
   likes: number,
   accounts: string[],
-  status: string,
-  details?: {}
+  status: "open" | "close",
+  details?: {createAt: Date; updateAt: Date;}
 }
 
 const page1: Page = {
